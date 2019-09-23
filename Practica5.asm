a60b9b987a2f9fcbc981941ca3676a1da8fab395
Sofia9711 <55411212+Sofia9711@users.noreply.github.com>
2019-09-23 13:26:00 -0500

    Update Practica5.asm

--- Practica5.asm
+++ Practica5.asm
@@ -1,23 +1,8 @@
    
 
-   RADIX	DEC		; SET DECIMAL AS DEFAULT BASE
-    PROCESSOR	18F45K50	; SET PROCESSOR TYPE
-    #INCLUDE	<p18f45k50.inc>
-    
-;;	***ONLY NEEDED FOR SOFTWARE SIMULATION***
-;
-   ORG	0			; RESET VECTOR
-   GOTO	0X1000
-;
-   ORG	0X08			; HIGH INTERRUPT VECTOR
-   GOTO	0X1008
-;
-   ORG	0X18			; LOW INTERRUPT VECTOR
-   GOTO	0X1018
-   
-   
-   AUX	    EQU	    0
-    
+  AUX		EQU    0
+   VARIABLE1	EQU    1
+   VARIABLE2	EQU    2
    
 
 
@@ -28,69 +13,75 @@ MAIN:
     CLRF    TRISB		; SET PORTB AS OUTPUT
     SETF    TRISC
     CLRF    LATB
-    BSF	    TRISC, 1
-    BSF	    LATB, 0
  
 LOOP:
     BTFSS   PORTC, 0 ;GRAY
     BRA	    RUTINA_A
-    CALL    DELAY
     BTFSS   PORTC, 1 ;IMPAR
     BRA	    RUTINA_B
-    CALL    DELAY
     BTFSS   PORTC, 2 ;PAR
     BRA	    RUTINA_C
+    BRA	    LOOP
     
 RURINA_A: ;RUTINA GRAY
-    BTFSC   PORTC, 3
-    CALL    B4
-    BTFSC   PORTC, 4
-    CALL    B5
+    BTFSS   PORTC, 3
+    INCF    AUX, 1
+    BTFSS   PORTC, 4
+    DECF    AUX, 1
+    BRA	    RUTINA_A
     
 RUTINA_B:;IMPAR
-    BTFSC   PORTC, 3
-    CALL    B4
-    BTFSC   PORTC, 4
-    CALL    B5
-    CLRF    LATB
+    MOVF    PORTB, 0
+    BTFSS   PORTC, 3
+    ADDLW   1
+    MOVWF   AUX, 1
+    BTFSS   PORTC, 4
+    SUBLW   1
+    MOVWF   AUX, 1
     BTFSC   AUX, 0
     MOVFF   AUX, LATB
-    BTFSC   PORTC, 1
+    BTFSS   AUX, 0
+    CLRF    LATB
+    CALL    DELAY
+    BTFSS   PORTC, 1
     BRA	    RUTINA_A
-    BTFSC   PORTC, 2
+    BTFSS   PORTC, 2
     BRA	    LOOP
-    BTFSC   PORTC, 3
+    BTFSS   PORTC, 3
     BRA	    RUTINA_C
     BRA	    RUTINA_B
     
 RUTINA_C:;PAR
-    BTFSC   PORTC, 3
-    CALL    B4
-    BTFSC   PORTC, 4
-    CALL    B5
-    CLRF    LATB
+    MOVF    PORTB, 0
+    BTFSS   PORTC, 3
+    ADDLW   1
+    MOVWF   AUX, 1
+    BTFSS   PORTC, 4
+    SUBLW   1
+    MOVWF   AUX, 1
     BTFSS   AUX, 0
     MOVFF   AUX, LATB
-    BTFSC   PORTC, 1
+    BTFSC   AUX, 0
+    CLRF    LATB
+    CALL    DELAY
+    BTFSS   PORTC, 1
     BRA	    RUTINA_A
-    BTFSC   PORTC, 2
+    BTFSS   PORTC, 2
     BRA	    RUTINA_B
-    BTFSC   PORTC, 3
+    BTFSS   PORTC, 3
     BRA	    LOOP
     BRA	    RUTINA_C
     
-B4: ; MÁS (AUMENTAR +1)
-    INCF    AUX, 1
-    CALL    DELAY
-    RETURN
-B5: ; MENOS (RESTAR -1)
-    DECF    AUX, 1 
-    CALL    DELAY
-    RETURN
-
+    
         
 DELAY:
     MOVLW   0xFF
     MOVWF   VARIABLE1
     MOVWF   VARIABLE2
+    
+LOOP1:
+    DECFSZ  VARIABLE2
+    GOTO    LOOP1
+    DECFSZ  VARIABLE1
+    GOTO    LOOP1
     RETURN
